ba5f8091c64f9026cc8841a6b614c63f8500ada7
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-27 17:47:28 -0500

    Init // comment

--- src/Marwan/MarwanPractice1.java
+++ src/Marwan/MarwanPractice1.java
@@ -3,6 +3,7 @@ package Marwan;
 public class MarwanPractice1 {
     public static void main(String[] args) {
         System.out.println("Good evening everyone");
+        // so far good
 
     }
 }
