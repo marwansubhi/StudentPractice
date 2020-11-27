9b59533e19a62f663d563f76f1953cab5911bff9
Ruslan <ruslan.nasirov@gmail.com>
2020-11-27 18:37:56 -0500

    added Q&A class and a question

---
+++ src/Ruslan/RuslanQAndA.java
@@ -0,0 +1,12 @@
+package Ruslan;
+
+public class RuslanQAndA {
+
+    public static void main(String[] args) {
+
+        /*
+        Would you be willing to do more of this anytime soon?
+        */
+
+    }
+}
