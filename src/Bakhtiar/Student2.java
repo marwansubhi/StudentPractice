f05b7eb83a6591ad0e24636a79f47670a5864d6c
Bakhtiar Semseddin <korshat@gmail.com>
2020-11-27 18:29:02 -0500

    I Am doing also

---
+++ src/Bakhtiar/Student2.java
@@ -0,0 +1,9 @@
+package Bakhtiar;
+
+public class Student2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hey, how are you");
+    }
+}
--- src/Marwan/Bahtiyar.java
+++
@@ -1,9 +0,0 @@
-package Marwan;
-
-public class Bahtiyar {
-
-    public static void main(String[] args) {
-
-        System.out.println("this is bakhtiyar");
-    }
-}
