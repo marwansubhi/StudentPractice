6f9457ce6de8610c61121f2e9b9704573c42878d
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-27 18:43:15 -0500

    added a new func to be discussed

--- src/Marwan/BranchPractice.java
+++ src/Marwan/BranchPractice.java
@@ -3,5 +3,8 @@ package Marwan;
 public class BranchPractice {
     public static void main(String[] args) {
         System.out.println("Good evening");
+        for (int i =1; i<2; i++){
+            System.out.println("good work");
+        }
     }
 }
